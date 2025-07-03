c2128c553b7421e4b24bdf1772ee5d95818176fa
eliatawil <165721132+eliatawil@users.noreply.github.com>
2025-07-03 08:28:17 -0300

    teste

--- public/script.js
+++ public/script.js
@@ -37,6 +37,13 @@ const textareaOutroMudanca = document.getElementById("mudanca_outro");
     }
   });
 
+const form = document.querySelector("form");
+  const loading = document.getElementById("loading");
+
+  form.addEventListener("submit", function () {
+    loading.style.display = "flex"; // ou "block"
+  });
+
 const selectMordida = document.getElementById("mordida");
 const textareaOutro = document.getElementById("mordida_outro");
 
